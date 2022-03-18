6812fa18e8b2a8be04832a0ce375a83f44e177df
jasonfritsche <fritsche.jason@gmail.com>
2022-03-17 21:51:10 -0500

    added created  plotoptions in chart component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from "@angular/core";
-import { IHighchartsOptions } from "src/interfaces/charts";
-import { IWordSearchOptions, IWordSearchResult } from "src/interfaces/words";
-import { HighchartsService } from "src/services/highcharts.service";
+import { Component } from '@angular/core';
+import { IHighchartsOptions } from 'src/interfaces/charts';
+import { IWordSearchOptions } from 'src/interfaces/words';
+import { HighchartsService } from 'src/services/highcharts.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = "Word Traverse";
+  title = 'Word Traverse';
   showChart = false;
   chartOptions!: IHighchartsOptions;
   latestSearch!: { word: string; searchOption: IWordSearchOptions };
@@ -23,7 +23,10 @@ export class AppComponent {
     });
   }
 
-  handleLatestSearch(latestSearch: { word: string; searchOption: IWordSearchOptions }) {
+  handleLatestSearch(latestSearch: {
+    word: string;
+    searchOption: IWordSearchOptions;
+  }) {
     this.latestSearch = latestSearch;
   }
 }
